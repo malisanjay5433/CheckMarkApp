56411bb5a0537ae1d7384167353e4d9336f4c103
Sanjay Mali <sanjay.mali@propstack.com>
2016-12-22 13:08:27 +0530

    final commit

--- CheckMarkApp/Check_Header.h
+++ CheckMarkApp/Check_Header.h
@@ -5,9 +5,4 @@
 //  Created by Sanjay Mali on 22/12/16.
 //  Copyright © 2016 Sanjay. All rights reserved.
 //
-
-#ifndef Check_Header_h
-#define Check_Header_h
-
-
-#endif /* Check_Header_h */
+#import "BEMCheckBox.h"
--- CheckMarkApp/ViewController.swift
+++ CheckMarkApp/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,BEMCheckBoxDelegate{
 
+    @IBOutlet weak var b1: BEMCheckBox!
+    @IBOutlet weak var b2: BEMCheckBox!
+    @IBOutlet weak var b3: BEMCheckBox!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        b1.delegate = self
+        b2.delegate = self
+        b3.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+   override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        b1.setOn(true, animated: true)
+    }
 
+    func didTap(_ checkBox: BEMCheckBox) {
+        if checkBox.tag == 0{
+            b1.onAnimationType = .fill
+            
+        }
+        if checkBox.tag == 1{
+            b1.onAnimationType = .bounce
+            
+        }
+        if checkBox.tag == 2{
+            b1.onAnimationType = .stroke
+            
+        }
+    }
 
 }
+
 
