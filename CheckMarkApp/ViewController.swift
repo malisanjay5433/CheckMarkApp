ea4789e9b9456edb09341a83ddd7098cad9167d5
Sanjay Mali <sanjay.mali@propstack.com>
2016-12-22 12:31:10 +0530

    Initial Commit

---
+++ CheckMarkApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckMarkApp
+//
+//  Created by Sanjay Mali on 22/12/16.
+//  Copyright © 2016 Sanjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
